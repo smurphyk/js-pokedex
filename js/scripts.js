5cd44dd716ae90a4b8bb8155e8d4e4288c45fde0
seankmurphy <56086375+smurphyk@users.noreply.github.com>
2020-04-17 11:59:24 -0500

    Task 1.3 created for loop to display pokemon names on DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,29 @@
 var pokemonList = [
   {
     name: 'Raichu',
-    height: 0.8+'m',
+    height: 0.8,
     types: ['Mouse', 'Electric'],
     abilities: ['Static', 'Lightningrod'],
   },
   {
     name: 'Charmeleon',
-    height: 1.1+'m',
+    height: 1.1,
     types: ['Flame', 'Fire'],
     abilities: ['Blaze', 'Solar-Power'],
   },
   {
     name: 'Alakazam',
-    height: 1.5+'m',
+    height: 1.5,
     types: ['Telekinesis', 'Psychic'],
     abilities: ['Synchronize', 'Inner-Focus', 'Magic-Guard'],
   },
   {
     name: 'Pidgeotto',
-    height: 1.1+'m',
+    height: 1.1,
     types: ['Bird', 'Flying', 'Normal'],
     abilities: ['Keen-Eye', 'Tangled-Feet', 'Big-Pecks'],
   },
 ];
-
-for (var pokemonList=0; i<pokemonList.length; i++) {
-  document.write(pokemonList.name);
+for (var i = 0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name);
 }
