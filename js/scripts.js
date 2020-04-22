0578c05ad6c09a463915686862d7fe486c3e0714
seankmurphy <56086375+smurphyk@users.noreply.github.com>
2020-04-22 14:46:21 -0500

    wrapped pokemonList in IIFE, added pokemonRepository variable, filled pokemonList array, and wrote it to index
    Working on cleaning up outside of the IIFE to make the code more efficient.

--- js/scripts.js
+++ js/scripts.js
@@ -1,38 +1,64 @@
-var pokemonList = [
+var pokemonRepository = (function() {
+  var pokemonList = [];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+      add: add,
+      getAll: getAll
+    };
+  })();
+
+var pokemonRaichu =
   {
     name: 'Raichu',
     height: 0.8,
     types: ['Mouse', 'Electric'],
     abilities: ['Static', 'Lightningrod'],
-  },
+  };
+var pokemonCharmeleon =
   {
     name: 'Charmeleon',
     height: 1.1,
     types: ['Flame', 'Fire'],
     abilities: ['Blaze', 'Solar-Power'],
-  },
+  };
+var pokemonAlakazam =
   {
     name: 'Alakazam',
     height: 1.5,
     types: ['Telekinesis', 'Psychic'],
     abilities: ['Synchronize', 'Inner-Focus', 'Magic-Guard'],
-  },
+  };
+var pokemonPidgeotto =
   {
     name: 'Pidgeotto',
     height: 1.1,
     types: ['Bird', 'Flying', 'Normal'],
     abilities: ['Keen-Eye', 'Tangled-Feet', 'Big-Pecks'],
-  },
-];
+  };
 
-/* for (var i = 0; i < pokemonList.length; i++) {
-  document.write('<h1>' + pokemonList[i].name + '</h1>' + '<h2>' + ' Height: ' + pokemonList[i].height)
-    if (pokemonList[i].height > 1.1) {
-      document.write(" - Wow, that's BIG! ")
-    }
-}; */
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add('<h1>' + pokemonRaichu.name + '</h1>' + '<h2>' + ' Height: ' + pokemonRaichu.height +'m');
 
-pokemonList.forEach(function(pokemon) {
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add('<h1>' + pokemonCharmeleon.name + '</h1>' + '<h2>' + ' Height: ' + pokemonCharmeleon.height +'m');
+
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add('<h1>' + pokemonAlakazam.name + '</h1>' + '<h2>' + ' Height: ' + pokemonAlakazam.height +'m');
+
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add('<h1>' + pokemonPidgeotto.name + '</h1>' + '<h2>' + ' Height: ' + pokemonPidgeotto.height +'m');
+  document.write(pokemonRepository.getAll());
+
+
+/*pokemonList.forEach(function(pokemon) {
   document.write('<h1>' + pokemon.name + '</h1>' + '<h2>' + ' Height: ' + pokemon.height +'m');
   if (pokemon.height > 1.1) {
     document.write(" - Wow, that's BIG! ")
@@ -40,4 +66,4 @@ pokemonList.forEach(function(pokemon) {
     var result = pokemon;
     return result;
   }
-});
+});*/
