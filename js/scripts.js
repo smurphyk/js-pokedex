6329ff76bc2be36f1d397084122f4082774e0a1c
seankmurphy <56086375+smurphyk@users.noreply.github.com>
2020-04-23 10:38:45 -0500

    assigned array of pokemon back to pokemonList variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
 var pokemonRepository = (function() {
-  var pokemonList = [];
+  var pokemonList = [
+    {
+      name: 'Raichu',
+      height: 0.8,
+      types: ['Mouse', 'Electric'],
+      abilities: ['Static', 'Lightningrod'],
+    },
+    {
+      name: 'Charmeleon',
+      height: 1.1,
+      types: ['Flame', 'Fire'],
+      abilities: ['Blaze', 'Solar-Power'],
+    },
+    {
+      name: 'Alakazam',
+      height: 1.5,
+      types: ['Telekinesis', 'Psychic'],
+      abilities: ['Synchronize', 'Inner-Focus', 'Magic-Guard'],
+    },
+    {
+      name: 'Pidgeotto',
+      height: 1.1,
+      types: ['Bird', 'Flying', 'Normal'],
+      abilities: ['Keen-Eye', 'Tangled-Feet', 'Big-Pecks'],
+    }
+  ];
 
   function add(pokemon) {
     pokemonList.push(pokemon);
@@ -14,49 +39,6 @@ var pokemonRepository = (function() {
       getAll: getAll
     };
   })();
-
-var pokemonRaichu =
-  {
-    name: 'Raichu',
-    height: 0.8,
-    types: ['Mouse', 'Electric'],
-    abilities: ['Static', 'Lightningrod'],
-  };
-var pokemonCharmeleon =
-  {
-    name: 'Charmeleon',
-    height: 1.1,
-    types: ['Flame', 'Fire'],
-    abilities: ['Blaze', 'Solar-Power'],
-  };
-var pokemonAlakazam =
-  {
-    name: 'Alakazam',
-    height: 1.5,
-    types: ['Telekinesis', 'Psychic'],
-    abilities: ['Synchronize', 'Inner-Focus', 'Magic-Guard'],
-  };
-var pokemonPidgeotto =
-  {
-    name: 'Pidgeotto',
-    height: 1.1,
-    types: ['Bird', 'Flying', 'Normal'],
-    abilities: ['Keen-Eye', 'Tangled-Feet', 'Big-Pecks'],
-  };
-
-  console.log(pokemonRepository.getAll());
-  pokemonRepository.add('<h1>' + pokemonRaichu.name + '</h1>' + '<h2>' + ' Height: ' + pokemonRaichu.height +'m');
-
-  console.log(pokemonRepository.getAll());
-  pokemonRepository.add('<h1>' + pokemonCharmeleon.name + '</h1>' + '<h2>' + ' Height: ' + pokemonCharmeleon.height +'m');
-
-  console.log(pokemonRepository.getAll());
-  pokemonRepository.add('<h1>' + pokemonAlakazam.name + '</h1>' + '<h2>' + ' Height: ' + pokemonAlakazam.height +'m');
-
-  console.log(pokemonRepository.getAll());
-  pokemonRepository.add('<h1>' + pokemonPidgeotto.name + '</h1>' + '<h2>' + ' Height: ' + pokemonPidgeotto.height +'m');
-  document.write(pokemonRepository.getAll());
-
 
 /*pokemonList.forEach(function(pokemon) {
   document.write('<h1>' + pokemon.name + '</h1>' + '<h2>' + ' Height: ' + pokemon.height +'m');
