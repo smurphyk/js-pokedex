7aeeaa3d74ffa3c666ebf571dfb22ad2e559736d
seankmurphy <56086375+smurphyk@users.noreply.github.com>
2020-05-01 13:58:45 -0500

    Completed Exercise 1.8 by creating a modal to display pokemon details

--- js/scripts.js
+++ js/scripts.js
@@ -52,11 +52,74 @@ var pokemonRepository = (function() {
     });
   }
 
+// Displays details in modal
   function showDetails(item) {
     pokemonRepository.loadDetails(item).then(function () {
-      console.log(item);
+      showModal(item);
     });
   }
+
+  // Displays Modal when pokemon is selected
+  function showModal(item) {
+    var $modalContainer = document.querySelector('#modal-container');
+    // Clear existing content first
+    $modalContainer.innerHTML = '';
+    // Add div to HTML for new modal content
+    var modal = document.createElement('div');
+    // Create a class for new div
+    modal.classList.add('pokemodal');
+    // Create a closing button for Modal
+    var closeButton = document.createElement('button');
+    closeButton.classList.add('closePokemon');
+    closeButton.innerText = 'X'
+    // Create event listener to close modal with closeButton is clicked
+    closeButton.addEventListener('click', hideModal);
+    // Display pokemon name in Modal
+    var pokeName = document.createElement('h1');
+    pokeName.classList.add('pokeName')
+    pokeName.innerText = item.name;
+    // Display pokemon image in Modal
+    var pokePic = document.createElement('img');
+    pokePic.classList.add('pokePic');
+    pokePic.setAttribute('src', item.imageUrl);
+    // Display pokemon height in Modal
+    var pokeHeight = document.createElement('h3');
+    pokeHeight.innerText = 'Height: ' + item.height +'m';
+
+    //Append Modal
+    modal.appendChild(closeButton);
+    modal.appendChild(pokeName);
+    modal.appendChild(pokePic);
+    modal.appendChild(pokeHeight);
+    $modalContainer.appendChild(modal);
+
+    // Create a class for showModal function
+    $modalContainer.classList.add('is-visible')
+  }
+
+  // Create hideModal function
+  function hideModal() {
+    var $modalContainer = document.querySelector('#modal-container');
+    $modalContainer.classList.remove('is-visible');
+  }
+
+  // Implement hideModal on outside mouse click
+  var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.addEventListener('click', e => {
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
+  // Implement hideModal on ESC key press
+  var $modalContainer = document.querySelector('#modal-container');
+  $modalContainer.addEventListener('keydown', e => {
+    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')
+  ) {
+      hideModal();
+  }
+});
 
   return {
       add: add,
